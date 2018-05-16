85765699ec8e86f9afb53871099d3674e61b9390
foxtail udon <foxtailudon@users.noreply.github.com>
2018-05-17 00:37:29 +0900

    first commit

---
+++ install.sh
@@ -0,0 +1,46 @@
+DOT_FILES=(.tmux.conf .vim .viminfo .vimrc .zprofile .zsh_history .zshenv .zshrc)
+
+usage_exit(){
+    echo "Usage $0 [-m MacOS] [-u Ubuntu]" 1>&2
+    exit 1
+}
+
+create_directory(){
+    for file in ${DOT_FILES[@]}
+    do 
+        ln -s $HOME/dotfiles/$file $HOME/$file
+    done
+}
+
+vim_install(){
+    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh -o /tmp/dein_installer.sh
+    sh /tmp/dein_installer.sh ~/dotfiles/.vim/bundle
+    rm /tmp/dein_installer.sh
+}
+
+
+while getopts mlh OPT
+do
+    case $OPT in
+        m) 
+            brew install tmux zsh 
+            pip3 install --upgrade pip
+            pip3 install powerline-status
+            create_directory
+            vim_install
+            ;;
+        u)
+            apt install tmux zsh
+            pip3 install --upgrade pip
+            pip3 install --user powerline-status
+            create_directory
+            vim_install
+            ;;
+        h) 
+            usage_exit
+            ;;
+        \?)
+            usage_exit
+            ;;
+    esac
+done
